c8287c16284f4aec5776e57062ce7cd2a4fd6d17
Eugen Schreiner <eugen.schreiner@bertelsmann.de>
2023-04-07 12:28:10 +0200

    include scss and png

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,12 @@
 import {handleSubmit} from './js/formHandler';
 import {checkForName} from './js/nameChecker';
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
+import './images/favicon.png';
 
 console.log(handleSubmit);
 console.log(checkForName);
--- webpack.prod.js
+++ webpack.prod.js
@@ -17,8 +17,22 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader"
-            }
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[name].[ext]'
+                    }
+                }]
+            },
         ]
     },
     plugins: [
